c6bdc7f00a7103b80ed728d19a38541647269311
Edily Mora <edilyrosa@gmail.com>
2026-07-30 15:54:29 -0400

    Fix CORS headers in Go

--- backend/main.go
+++ backend/main.go
@@ -31,8 +31,9 @@ func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
+		// Esto es vital: responde inmediatamente a la petición previa del navegador (OPTIONS)
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
